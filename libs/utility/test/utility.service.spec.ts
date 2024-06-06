31015a674fce31f616bf7c73f3bfd63b719394d2
zoltan.szabo <zoltan.szabo.it@gmail.com>
2024-06-06 17:02:03 +0200

    move application structure from nodejs-workspaces-powered-project

---
+++ libs/utility/test/utility.service.spec.ts
@@ -0,0 +1,26 @@
+import {Logger} from "tslog";
+import {UtilityService} from "../src/utility.service";
+
+const logger = new Logger({ type: 'pretty' });
+
+describe('UtilityService', () => {
+  beforeAll(() => {
+  });
+
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('start server', async () => {
+    // given
+    const utilityService: UtilityService = new UtilityService();
+
+    // when
+    let result = utilityService.calculate(10, 20);
+
+    // then
+    expect(result).toBeDefined();
+    expect(result).toEqual(30);
+  });
+})
+
